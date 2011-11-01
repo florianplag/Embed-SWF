f019280e5761e5d4d8752945bc519a4f80775648
Florian Plag <mail@florian-plag.de>
2011-11-01 18:16:33 +0100

    Added a list of old flash player major releases with their main features

--- flash-player-version.php
+++ flash-player-version.php
@@ -9,7 +9,7 @@ $pageId = 3;
 		<meta name="description" content="This page shows your currently installed Flash player version.">
 		<meta name="author" content="">
 		<?php
-			include "php/js-includes.php";
+		include "php/js-includes.php";
 		?>
 
 		<script src="jslibs/swfobject.js"></script>
@@ -18,7 +18,7 @@ $pageId = 3;
 	<body itemscope itemtype="http://schema.org/Article">
 		<!-- ::::::::::::::: TOP ::::::::::::::::::::: -->
 		<?php
-			include "php/topnavi.php";
+		include "php/topnavi.php";
 		?>
 
 		<!-- ::::::::::::::: CONTAINER ::::::::::::::::::::: -->
@@ -43,7 +43,7 @@ $pageId = 3;
 					<div class="span12">
 						<h2 itemprop="name">Check your Flash Player Version</h2>
 						<p itemprop="description">
-							This page shows your currently installed Flash player version. It's uses the player detection written in JavaScript of <em>SWFObject</em>. Note: Only the first 3 numbers shown, because in most browsers the 4th number is not detectable.
+							This page shows your currently installed Flash player version. It's uses the player detection written in JavaScript of <em>SWFObject</em>. Note: Only the first 3 numbers shown, because in most browsers the 4th number of the installed Flash Player version is not detectable.
 						</p>
 						<div class="alert-message block-message info" style="font-size:1.2em; margin-top: 20px;margin-bottom: 30px;">
 							Your current Flash Player Version is: <strong id="resultTxt"></strong>
@@ -60,14 +60,27 @@ $pageId = 3;
 							</thead>
 							<tbody id="playerVersionTable"></tbody>
 						</table>
+						<p>
 						<small>Source: <a href="http://www.adobe.com/software/flash/about/">www.adobe.com</a></small>
+						</p>						
+						<h2>Old Flash Player Versions</h2>
+						<p>The following lists show the major releases of the Flash Player with some of their main features.</p>
+						<ul>
+							<li><strong>Flash Player 10.3</strong> (10.3.181.14 / May 2011)<ul><li>Media Measurement</li><li>Acoustic Echo Cancellation (desktop only)</li><li>Local Storage in privacy mode</li><li>Native Control Panel</li></ul></li>
+							<li><strong>Flash Player 10.2</strong> (10.2.152.26 / February 2011)<ul><li>Stage Video</li><li>Custom native mouse cursors</li><li>Multiple monitor full-screen support</li></ul></li>
+							<li><strong>Flash Player 10.1</strong> (10.1.53.64 / June 2010)<ul><li>Hardware-based H.264 video decoding</li><li>HTTP Dynamic Streaming</li><li>Peer-assisted networking and Multicast</li><li>Multi-touch APIs</li><li>Support for browser privacy modes</li></ul></li>
+							<li><strong>Flash Player 10</strong> (10.0.12.36 / October 2008)<ul><li>3D object transformations</li><li>pixel bender</li><li>speex Audio Codec</li><li>RTMFP</li><li>vector data type</li></ul></li>
+							<li><strong>Flash Player 9</strong> (9.0.28.0 / November 2006)<ul><li>Official MPEG-4 Video Support (Codecs: H.264 + AAC)</li><li>hardware Acceleration</li><li>multicore Support</li></ul></li>
+							<li><strong>Flash Player 8</strong> (8.0.22.0 / August 2005)<ul><li>Video Codec On2 VP6</li><li>New text rendering engine</li><li>Live filters</li><li>Blendmodes</li></ul></li>
+						</ul>				
 					</div>
 				</div>
-			</div>	<!-- /CONTENT -->
+			</div>
+			<!-- /CONTENT -->
 		</div>
 		<!-- /container -->
 		<?php
-			include "php/footer.php";
+		include "php/footer.php";
 		?>
 	</body>
 </html>
